0aeee514f4b72073858771fad1489418bdcac3f1
amg2062 <atharv2062@gmail.com>
2022-06-24 14:35:22 +0530

    first commit

---
+++ src/components/Header.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Header(){
+    return(
+        <div className="header-style">
+            <p className="header-font">VitTube</p>
+            <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT3d8Bwurtm2r56mT3y79-sm8AD8F80VmBakQ&usqp=CAU"/>
+        </div>
+    );
+}
+export default Header;
--- src/index.js
+++ src/index.js
@@ -1,17 +1,50 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import _ from "lodash";
+import React ,{Component} from 'react';
+import ReactDOM from 'react-dom';
+import YTSearch from 'youtube-api-search';
+import SearchBar from "./components/SearchBar";
+import VideoDetail from "./components/VideoDetail";
+import VideoList from "./components/VideoList";
+import Header from "./components/Header";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+const API_KEY='AIzaSyB8QCxy1DaccIR7HsPrEgVvetaef-PlUdc';
+class MyApp extends Component{
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+  constructor(props){
+    super(props);
+
+    this.state={
+      videos:[],
+      selectedVideo:null
+    };
+    this.videoSearch("");
+
+  }
+  videoSearch(term){
+    YTSearch({key: API_KEY, term:term},(videos)=>{
+      this.setState({
+        videos:videos,
+        selectedVideo:videos[0]
+      });
+    });
+  }
+  render(){
+    const videoSearch= _.debounce((term)=>{this.videoSearch(term)},300);
+    return(
+      <div className="main-div">
+        <Header/>
+        <SearchBar onSearchTermChange={videoSearch}/>
+        
+        <VideoDetail video={this.state.selectedVideo} className="main-div"/>
+        <VideoList
+          onVideoSelect={selectedVideo=> this.setState({selectedVideo})}
+          videos={this.state.videos}
+          className="main-div"
+        />      
+      </div>
+    );
+  }
+}
+ReactDOM.render(<MyApp/>,document.querySelector('.root'));
+
+
